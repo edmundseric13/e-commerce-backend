789ce42282948e0d6afd1ace2f6fb55529599fad
edmundseric13 <160555371+edmundseric13@users.noreply.github.com>
2024-07-29 13:58:28 -0600

    implementing sequelize

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      dialect: process.env.DB_DIALECT || 'postgres',
       dialectOptions: {
         decimalNumbers: true,
       },
